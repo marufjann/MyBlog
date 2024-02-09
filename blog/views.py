0d36d657846ba260540cb0b372eaa8d1f0c5aab9
Samatov Marufjon <m.samatov@orienbank.tj>
2024-02-09 13:42:01 +0500

    add function tags

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,17 @@ from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -20,7 +27,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -35,10 +43,16 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'form': form})
+                                                     'form': form,
+                                                     'similar_posts': similar_posts})
 
 
 # class PostListView(ListView):
